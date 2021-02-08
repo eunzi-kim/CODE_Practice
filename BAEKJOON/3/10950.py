1e6893af8a419795833872692710fedab524775e
eunzi-kim <rladmswlek@naver.com>
2021-02-08 23:56:59 +0900

    Add 0208 baekjoon solution plus

---
+++ BAEKJOON/3/10950.py
@@ -0,0 +1,9 @@
+# A + B 
+
+T = int(input())
+
+for test_case in range(1, T+1):
+    a, b = map(int, input().split())
+    result = a + b 
+
+    print(result)
---
+++ BAEKJOON/3/2739.py
@@ -0,0 +1,8 @@
+# 구구단
+
+T = int(input())
+
+for x in range(1, 10):
+    result = T * x
+
+    print(f'{T} * {x} = {result}') 
