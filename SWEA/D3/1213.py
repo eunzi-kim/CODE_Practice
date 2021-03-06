83ce4658da27d0c936f36539a6d7f79fd031328b
eunzi-kim <rladmswlek@naver.com>
2021-03-06 23:57:33 +0900

    Add 0306 problem solving practice

---
+++ SWEA/D3/1213.py
@@ -0,0 +1,19 @@
+# String
+
+import sys
+sys.stdin = open("input.txt", "r", encoding="UTF-8")
+
+for TC in range(1, 11):
+    T = int(input())
+    Word = input()
+    String = input()
+    count = 0
+    # 찾는 문자열 첫 글자
+    for i in range(len(String)-len(Word)+1):
+        letter = ""
+        # 문자열 탐색
+        for j in range(i, i+len(Word)):
+            letter += String[j]
+        if letter == Word:
+            count += 1
+    print("#{} {}".format(TC, count))
