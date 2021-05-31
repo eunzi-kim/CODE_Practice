18d5d1d2e335313e9eff489c23dcc68549586782
eunzi-kim <rladmswlek@naver.com>
2021-05-31 14:24:03 +0900

    Add 0531 algorithm study

--- 프로그래머스/체육복.py
+++ 프로그래머스/체육복.py
@@ -1,11 +1,12 @@
 def solution(n, lost, reserve):
-    # 결과값 초기화
-    answer = -1
     Cloth = [1] * (n+1)
+    # 여벌의 체육복 입력
     for x in reserve:
         Cloth[x] += 1
+    # 도난 체육복 입력
     for x in lost:
         Cloth[x] -= 1
+    # 체육복 check!
     for i in range(1, n+1):
         # i번째 학생이 체육복이 없음
         if Cloth[i] < 1:
@@ -15,7 +16,8 @@ def solution(n, lost, reserve):
             elif i+1 <= n and Cloth[i+1] > 1:
                 Cloth[i] += 1
                 Cloth[i+1] -= 1
-
+    # 결과값 초기화
+    answer = -1
     for x in Cloth:
         if x > 0:
             answer += 1
